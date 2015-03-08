1369534049fa3ef3df4a299523da47596045e057
HirokiMiyakura <miyakurahiroki@gmail.com>
2015-03-08 23:47:55 +0900

    design

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+  	@articles = Article.all.limit(5)
+  	@germany = Article.where(topic: 'ブンデスリーガ')
+  	@england = Article.where(topic: 'プレミアリーグ')
+  	@spain = Article.where(topic: 'リーガエスパニョル')
+  	@italy = Article.where(topic: 'セリエA')
+
+  	@user = current_user
   end
 
   def help
--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,10 @@ Rails.application.routes.draw do
   	end
   end
   resources :sessions, only: [ :new, :create, :destroy ]
-  resources :articles, only: [ :index, :new, :create, :destroy ]
+  resources :articles
   resources :relationships, only: [ :create, :destroy ]
   root 'static_pages#home'
   match '/signup', to: 'users#new', via: 'get'
-  match '/signin', to: 'sessions#new', via: 'new'
+  match '/signin', to: 'sessions#new', via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete'
 end
